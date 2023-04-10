8ce72f3ccb62fb3f30665106514e91fb8b7f4398
Soumya2411 <104072819+Soumya2411@users.noreply.github.com>
2023-04-11 00:32:08 +0530

    State1

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,29 @@
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css'
-import React from 'react';
+import React,{useState} from 'react';
 import Card from '../UI/Card'
+
+
+
 function ExpenseItem(props){
+//first is the initial value,second is the constant will give new value
+//it always return array with exact two values
+    const [title,setTitle] = useState(props.title);
+
     
+    const clickHandler=()=>{
+        
+setTitle('Updated');
+console.log(title);
+    }
     return(
         <Card className='expense-item'>
        <ExpenseDate date={props.date}></ExpenseDate>
         <div className='expense-item__description'>
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
 </div>
-<button onClick={()=> {console.log('clicked')}}>Change Title</button>
+<button onClick={clickHandler}>Change Title</button>
 </Card>
 );
 }
