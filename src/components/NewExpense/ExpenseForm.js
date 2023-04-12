1d09376e49e5d53499542811b7ac5ec0fa806a9d
Soumya2411 <104072819+Soumya2411@users.noreply.github.com>
2023-04-12 23:51:31 +0530

    merging states

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -3,25 +3,42 @@ import './ExpenseForm.css'
 
 const ExpenseForm=()=>{
 
-const [enteredTitle,setEnteredTitle]= useState('')
+// const [enteredTitle,setEnteredTitle]= useState('')
 
-const [enteredAmount,setEnteredAmount]= useState('')
+// const [enteredAmount,setEnteredAmount]= useState('')
 
-const [enteredDate,setEnteredDate]= useState('')
-
+// const [enteredDate,setEnteredDate]= useState('')
+const [userInput,setUserInput]= useState({
+enteredTitle:'',
+enteredAmount:'',   
+enteredDate:''
+});
 
 
     const titleChangeHandler = (event) =>{
-        setEnteredTitle(event.target.value);
+        // setEnteredTitle(event.target.value);
+        setUserInput({
+            ...userInput,
+            enteredTitle:event.target.value,
+
+    });
     };
 
 const amountChangeHandler=(event)=> {
-    setEnteredAmount(event.target.value)
-}
+    // setEnteredAmount(event.target.value)
+    setUserInput({
+        ...userInput,
+        enteredAmount:event.target.value,
+});
+};
 
 const dateChangeHandler = (event) =>{
-    setEnteredDate(event.target.valuel)
-}
+    // setEnteredDate(event.target.valuel)
+    setUserInput({
+        ...userInput,
+        enteredDate:event.target.value,
+});
+};
 
 
 return (
