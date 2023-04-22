205acc7aed55d95670b0addf3cbbb3ebb273b38f
Soumya2411 <104072819+Soumya2411@users.noreply.github.com>
2023-04-22 21:47:59 +0530

    update

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,27 +1,29 @@
+import React, { useState } from 'react';
+
 import ExpenseDate from './ExpenseDate';
-import './ExpenseItem.css'
-import React,{useState} from 'react';
-import Card from '../UI/Card'
+import Card from '../UI/Card';
+import './ExpenseItem.css';
 
-
-
-function ExpenseItem(props){
-//first is the initial value,second is the constant will give new value
-//it always return array with exact two values
-    const [title,setTitle] = useState(props.title); 
-    const clickHandler=()=>{       
-    setTitle('Updated');
+const ExpenseItem = (props) => {
+  // function clickHandler() {}
+  const [title, setTitle] = useState(props.title);
+  console.log('ExpenseItem evaluated by React');
+  
+  const clickHandler = () => {
+    setTitle('Updated!');
     console.log(title);
-    }
-    return(
-        <Card className='expense-item'>
-       <ExpenseDate date={props.date}></ExpenseDate>
-        <div className='expense-item__description'>
+  };
+
+  return (
+    <Card className='expense-item'>
+      <ExpenseDate date={props.date} />
+      <div className='expense-item__description'>
         <h2>{title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
-</div>
-<button onClick={clickHandler}>Change Title</button>
-</Card>
-);
+      </div>
+      <button onClick={clickHandler}>Change Title</button>
+    </Card>
+  );
 }
+
 export default ExpenseItem;
--- src/components/Expenses/ExpensesFilter.js
+++ src/components/Expenses/ExpensesFilter.js
@@ -1,30 +1,26 @@
 
-import React, { useState } from "react";
+import React from 'react';
 
 import './ExpensesFilter.css';
 
 const ExpensesFilter = (props) => {
-  const [selectedvalue, setSelectedvalue] = useState("2019");
-
-  const selectionHandler = (event) => {
-    props.onFilter(event.target.value);
-    setSelectedvalue(event.target.value);
-    
+  const dropdownChangeHandler = (event) => {
+    props.onChangeFilter(event.target.value);
   };
+
   return (
-    <div className="expenses-filter">
-      <div className="expenses-filter__control">
+    <div className='expenses-filter'>
+      <div className='expenses-filter__control'>
         <label>Filter by year</label>
-        <select value={selectedvalue} onChange={selectionHandler}>
-          <option value="2022">2022</option>
-          <option value="2021">2021</option>
-          <option value="2020">2020</option>
-          <option value="2019">2019</option>
+        <select value={props.selected} onChange={dropdownChangeHandler}>
+          <option value='2022'>2022</option>
+          <option value='2021'>2021</option>
+          <option value='2020'>2020</option>
+          <option value='2019'>2019</option>
         </select>
       </div>
     </div>
   );
 };
-
 
 export default ExpensesFilter;
