684e966fa5e148a34bc5822a9cbf4cf39df2ba40
Soumya2411 <104072819+Soumya2411@users.noreply.github.com>
2023-04-23 23:06:06 +0530

    import

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -18,10 +18,12 @@ const Expenses = (props) => {
     <div>
       <Card className='expenses'>
         <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+        {props.items.map(expense=><ExpenseItem title={expense.title} amount={expense.amount} date={expense.date}/>)}
         <ExpenseItem
           title={props.items[0].title}
           amount={props.items[0].amount}
           date={props.items[0].date}
+
         />
         <ExpenseItem
           title={props.items[1].title}
