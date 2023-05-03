7e461b9c95207f9e29e47ff0957a6cadaaadf086
Soumya2411 <104072819+Soumya2411@users.noreply.github.com>
2023-05-03 23:59:57 +0530

    Update Expenses.js

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -19,6 +19,7 @@ const Expenses = (props) => {
       <Card className='expenses'>
         <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
         {props.items.map(expense=><ExpenseItem title={expense.title} amount={expense.amount} date={expense.date}/>)}
+        
         <ExpenseItem
           title={props.items[0].title}
           amount={props.items[0].amount}
